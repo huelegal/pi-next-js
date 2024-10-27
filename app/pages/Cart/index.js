5da8a7da082e141df458f6d1ccf97d38a14309c9
huelegal <vitorlegalvh@gmail.com>
2024-10-27 19:14:58 -0300

    cocada

--- app/pages/Cart/index.js
+++ app/pages/Cart/index.js
@@ -1,8 +1,10 @@
 "use client";
 
 import MiniProductCard from "@/app/components/MiniProductCard";
-import styles from "./styles.module.scss";
+import Header from "@/app/components/Header";
 import { useState } from "react";
+import styles from "./styles.module.scss";
+import Footer from "@/app/components/Footer";
 
 export default function Cart() {
   const [shipping, setShipping] = useState("");
@@ -35,77 +37,86 @@ export default function Cart() {
   };
 
   return (
-    <div className={styles.wrapper}>
-      <div className={styles.cart}>
-        <div className={styles.productContent}>
-          <div className={styles.title}>
-            <h1>Carrinho de compras</h1>
-            <h1>{products.length} itens</h1>
+    <>
+      <Header />
+
+      <div className={styles.wrapper}>
+        <div className={styles.cart}>
+          <div className={styles.productContent}>
+            <div className={styles.title}>
+              <h1>Carrinho de compras</h1>
+              <h1>{products.length} Itens</h1>
+            </div>
+
+            <div className={styles.labels}>
+              <span>Detalhes do produto</span>
+              <span>Quantidade</span>
+              <span>Preço</span>
+              <span>Total</span>
+            </div>
+
+            <div className={styles.productList}>
+              {products.map((product, index) => (
+                <MiniProductCard
+                  key={index}
+                  details={product.details}
+                  price={product.price}
+                  type={product.type}
+                  quantity={product.quantity}
+                  onQuantityChange={(newQuantity) =>
+                    updateQuantity(index, newQuantity)
+                  }
+                  onRemove={() => handleRemove(index)}
+                />
+              ))}
+            </div>
           </div>
 
-          <div className={styles.labels}>
-            <span>Detalhes do produto</span>
-            <span>Quantidade</span>
-            <span>Preço</span>
-            <span>Total</span>
-          </div>
+          <div className={styles.sidebar}>
+            <h1 className={styles.sideTitle}>Sumário</h1>
+            <div className={styles.summaryItem}>
+              <span>{products.length} itens</span>
+              <span>R$ {calculateTotal()}</span>
+            </div>
 
-          <div className={styles.productList}>
-            {products.map((product, index) => (
-              <MiniProductCard
-                key={index}
-                details={product.details}
-                price={product.price}
-                type={product.type}
-                quantity={product.quantity}
-                onQuantityChange={(newQuantity) =>
-                  updateQuantity(index, newQuantity)
-                }
-                onRemove={() => handleRemove(index)}
+            <div className={styles.inputGroup}>
+              <label htmlFor="shipping">Transportadora:</label>
+              <select
+                id="shipping"
+                value={shipping}
+                onChange={(e) => setShipping(e.target.value)}
+              >
+                <option value="Transportadora A">Transportadora A</option>
+                <option value="Transportadora B">Transportadora B</option>
+                <option value="Transportadora C">Transportadora C</option>
+              </select>
+            </div>
+
+            <div className={styles.inputGroup}>
+              <label htmlFor="promoCode">Código promocional:</label>
+              <input
+                type="text"
+                id="promoCode"
+                value={promoCode}
+                onChange={(e) => setPromoCode(e.target.value)}
+                placeholder="Digite seu código"
               />
-            ))}
-          </div>
-        </div>
+            </div>
 
-        <div className={styles.sidebar}>
-          <h2>Resumo da compra</h2>
-          <div className={styles.summaryItem}>
-            <span>{products.length} itens</span>
-            <span>R$ {calculateTotal()}</span>
-          </div>
-
-          <div className={styles.inputGroup}>
-            <label htmlFor="shipping">Transportadora:</label>
-            <select
-              id="shipping"
-              value={shipping}
-              onChange={(e) => setShipping(e.target.value)}
-            >
-              <option value="Transportadora A">Transportadora A</option>
-              <option value="Transportadora B">Transportadora B</option>
-              <option value="Transportadora C">Transportadora C</option>
-            </select>
-          </div>
-
-          <div className={styles.inputGroup}>
-            <label htmlFor="promoCode">Código promocional:</label>
-            <input
-              type="text"
-              id="promoCode"
-              value={promoCode}
-              onChange={(e) => setPromoCode(e.target.value)}
-              placeholder="Digite seu código"
-            />
-          </div>
-
-          <div className={styles.totalCheckout}>
-            <h3>Total: R$ {calculateTotal()}</h3>
-            <button onClick={handleCheckout} className={styles.checkoutButton}>
-              Checkout
-            </button>
+            <div className={styles.totalCheckout}>
+              <h3>Total: R$ {calculateTotal()}</h3>
+              <button
+                onClick={handleCheckout}
+                className={styles.checkoutButton}
+              >
+                Checkout
+              </button>
+            </div>
           </div>
         </div>
       </div>
-    </div>
+
+      <Footer />
+    </>
   );
 }
