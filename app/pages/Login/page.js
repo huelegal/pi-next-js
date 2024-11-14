324b55da5be14f4c895ba8efcd723630f73700ac
WilliamAraujo777 <will.araujo777@gmail.com>
2024-11-14 00:01:32 -0300

    Adicionando integração com API

--- app/pages/Login/page.js
+++ app/pages/Login/page.js
@@ -7,12 +7,59 @@ import styles from "./styles.module.scss";
 export default function Login({ onClose }) {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const [loginStatus, setLoginStatus] = useState(null); // Estado para armazenar o retorno HTTP
   const formRef = useRef(null);
   const modalRef = useRef(null); // Ref para o contêiner do modal
 
-  const handleSubmit = (e) => {
+  const handleSubmit = async (e) => {
     e.preventDefault();
-    console.log("Email:", email, "Password:", password);
+  
+    try { 
+      const response = await fetch("http://localhost:8093/api/users/login", { // Substitua pela URL da sua API
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ email, password }),
+      });
+  
+      // Verifica se o tipo de conteúdo da resposta é JSON antes de tentar parseá-lo
+      const contentType = response.headers.get("content-type");
+      let data;
+  
+      if (contentType && contentType.includes("application/json")) {
+        data = await response.json();
+      } else {
+        data = await response.text(); // Alternativa para casos de erro em outro formato
+      }
+  
+      setLoginStatus(response.status); // Armazena o status HTTP
+      console.log("Resposta:", data);
+  
+      if (response.ok) {
+        console.log("Login bem-sucedido");
+
+              // Cria uma string formatada com as informações para o alert
+            const userInfo = `
+            ID: ${data.id}
+            Nome: ${data.name}
+            Email: ${data.email}
+            Endereço: ${data.address.street}, ${data.address.number}, ${data.address.complement}
+            Bairro: ${data.address.neighborhood}
+            Cidade: ${data.address.city}
+            Estado: ${data.address.state}
+            CEP: ${data.address.postalCode}
+            País: ${data.address.country}
+          `;
+        
+          // Exibe um alerta com as informações do usuário
+          alert(userInfo);
+      } else {
+        alert(data);
+      }
+    } catch (error) {
+      alert("Erro nos servidor")
+    }
   };
 
   // Efeito para fechar o modal ao clicar fora dele
